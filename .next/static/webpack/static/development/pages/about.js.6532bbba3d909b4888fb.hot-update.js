515b02807012210d947b7191784af5c37047a990
Carlo Cadiz <carlocadiz71@gmail.com>
2020-03-06 11:00:24 -0500

    initial pages created

---
+++ .next/static/webpack/static/development/pages/about.js.6532bbba3d909b4888fb.hot-update.js
@@ -0,0 +1,97 @@
+webpackHotUpdate("static/development/pages/about.js",{
+
+/***/ "./components/Image.js":
+/*!*****************************!*\
+  !*** ./components/Image.js ***!
+  \*****************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "/Users/carlomagno/next-ssr/components/Image.js";
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+
+var Image = function Image() {
+  return __jsx("img", {
+    src: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fichef.bbci.co.uk%2Fnews%2F410%2Fcpsprodpb%2F025B%2Fproduction%2F_85730600_monkey2.jpg&imgrefurl=https%3A%2F%2Fwww.bbc.com%2Fnews%2Fworld-asia-34346092&tbnid=X8KfHZhMUTEIoM&vet=12ahUKEwid8ozvmYboAhVHTqwKHcRyC3MQMygFegUIARCVAg..i&docid=mqfeOd7UrDU3fM&w=410&h=410&q=monkey&ved=2ahUKEwid8ozvmYboAhVHTqwKHcRyC3MQMygFegUIARCVAg",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 2
+    },
+    __self: this
+  });
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Image);
+
+/***/ }),
+
+/***/ "./pages/about.js":
+/*!************************!*\
+  !*** ./pages/about.js ***!
+  \************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! next/link */ "./node_modules/next/link.js");
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(next_link__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _components_Image__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../components/Image */ "./components/Image.js");
+var _jsxFileName = "/Users/carlomagno/next-ssr/pages/about.js";
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+
+
+
+var About = function About() {
+  return __jsx("div", {
+    style: {
+      fontSize: '20px',
+      color: 'blue'
+    },
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 6
+    },
+    __self: this
+  }, __jsx("h1", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 7
+    },
+    __self: this
+  }, "About"), __jsx(next_link__WEBPACK_IMPORTED_MODULE_1___default.a, {
+    href: "/",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 8
+    },
+    __self: this
+  }, __jsx("button", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 9
+    },
+    __self: this
+  }, "Back")), __jsx(_components_Image__WEBPACK_IMPORTED_MODULE_2__["default"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 11
+    },
+    __self: this
+  }));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (About);
+
+/***/ })
+
+})
+//# sourceMappingURL=about.js.6532bbba3d909b4888fb.hot-update.js.map
---
+++ components/image.js
@@ -0,0 +1,6 @@
+const Image = () =>(
+  <img src= 'https://ichef.bbci.co.uk/news/320/cpsprodpb/025B/production/_85730600_monkey2.jpg'/>
+)
+
+export default Image;
+
